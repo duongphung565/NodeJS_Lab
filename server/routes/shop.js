a8621ab1329bd891e1bc708020d800e8b470437d
duongphung565 <nguyenphungduong565@gmail.com>
2022-12-14 00:54:19 +0700

    ok lab7

--- server/routes/shop.js
+++ server/routes/shop.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const express = require('express');
 const shopController = require('../controllers/shop.js');
+
 const router = express.Router();
 router.get('/', shopController.getProducts);
+router.get('/add-product', shopController.getAddProduct);
+router.post('/add-product', shopController.postAddProduct);
+
 module.exports = router;
